5563550e758b17efa34251f6674068af40c63452
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-04-09 17:44:22 +0200

    Remove `any`

--- sample.ts
+++ sample.ts
@@ -2,7 +2,7 @@ let { Prism, Token } = await import("prismjs/src/core.ts");
 let { default: javascript } = await import("prismjs/src/languages/prism-javascript.ts");
 
 const prism = new Prism();
-prism.components.add(javascript as any);
+prism.components.add(javascript);
 
 const tokens = prism.tokenize(
   `var foo = 0;`,
