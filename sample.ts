bbcf9257aa84a6ef9122963422c33acffbaadfaf
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-04-09 17:40:45 +0200

    An attempt to make it work

--- sample.ts
+++ sample.ts
@@ -1,12 +1,19 @@
-import { Prism, Token } from "prismjs";
+let { Prism, Token } = await import("prismjs/src/core.ts");
+let { default: javascript } = await import("prismjs/src/languages/prism-javascript.ts");
 
 const prism = new Prism();
+prism.components.add(javascript as any);
+
 const tokens = prism.tokenize(
   `var foo = 0;`,
   prism.components.getLanguage("javascript")!
 );
-tokens.forEach((token: Token | string) => {
+
+tokens.forEach((token) => {
   if (token instanceof Token && token.type === "number") {
     console.log(`Found numeric literal: ${token.content}`);
   }
 });
+
+// Mandatory. Otherwise, top-level await will not work.
+export {};
