dab7e06749744cd22fa270e609848d44d4a6cba3
Robert Webb <robertwebb@modular.com>
2025-04-10 10:27:42 -0700

    Working now

--- sample.ts
+++ sample.ts
@@ -1,12 +1,17 @@
-import { Prism, Token } from "prismjs";
+import { Token, Prism } from "prismjs";
+import javascript from "prismjs/languages/prism-javascript";
 
-const prism = new Prism();
-const tokens = prism.tokenize(
-  `var foo = 0;`,
-  prism.components.getLanguage("javascript")!
-);
-tokens.forEach((token: Token | string) => {
-  if (token instanceof Token && token.type === "number") {
-    console.log(`Found numeric literal: ${token.content}`);
-  }
-});
+async function main() {
+  const prism = new Prism();
+  prism.components.add(javascript);
+  const tokens = prism.tokenize(
+    `var foo = 0;`,
+    prism.components.getLanguage("javascript")
+  );
+  tokens.forEach((token: Token | string) => {
+    if (token instanceof Token && token.type === "number") {
+      console.log(`Found numeric literal: ${token.content}`);
+    }
+  });
+}
+main();
